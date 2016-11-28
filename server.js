1ca7f7856119117b83c4219ffc830fd661a30e9e
ponzanaja <ponza2539@hotmail.com>
2016-11-29 04:48:14 +0700

    แก้ไขเพิ่มเติม Final

--- server.js
+++ server.js
@@ -9,22 +9,54 @@ var config = {
     storageBucket: "userdatabase-71afb.appspot.com",
     messagingSenderId: "49200232033"
 };
-
+var data = []
+var data2 =[1,2,3,4,5,6];
 var port = process.env.PORT || 7777;
 firebase.initializeApp(config);
 var Users = firebase.database().ref('users')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+Users.on('value', function(snapshot){
+      snapshot.forEach(function (childSnap){
+        var value = childSnap.val();
+        if(value.UID === '939326652838978')
+        {
+          data.push(value)
+          //console.log(data);
+        }
+
+      });
+
+  //var x = users.find(user => user.UID === id)
+
+});
+Users.on('child_added', function(snapshot){
+  data.push(snapshot.val())
+  var x = data.find(user => user.UID === 12312412412)
+  if(x)
+  {
+    console.log("Hello world");
+  }
+  console.log(x);
+});
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello Node.js</h1>');
 });
 app.get('/index', function (req, res) {
     res.send('<h1>This is index page</h1>');
-    var data = {
+
+
+
+    /*var data = {
       UID : 12312412412,
       follower : [" "],
       state : "1"
     }
-      Users.push(data)
+      Users.push(data)*/
 });
 app.listen(port, function() {
     console.log('Starting node.js on port ' + port);
