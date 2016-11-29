4efa267f4178f1d4afbae88dc0337dedf8d1b3ef
ponzanaja <ponza2539@hotmail.com>
2016-11-29 17:34:08 +0700

    Add Add lisintner child , Change child

--- server.js
+++ server.js
@@ -18,6 +18,29 @@ var Users = firebase.database().ref('users')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+Users.on('child_changed', function(snapshot) {
+  var x = snapshot.val().UID
+  var index =data.findIndex(datas => datas.UID == x )
+  data.splice(index,1);
+  data.push(snapshot.val());
+  data.sort();
+  initData();
+});
+Users.on('child_added', function(snapshot){
+  data.push(snapshot.val())
+  });
+
+
+initData();
+
+
+
+/*var data3 = {
+  UID : '12312412412',
+  follower : [" "],
+  state : "1"
+}
+  Users.push(data3)
 
 Users.on('value', function(snapshot){
       snapshot.forEach(function (childSnap){
@@ -25,7 +48,7 @@ Users.on('value', function(snapshot){
         if(value.UID === '939326652838978')
         {
           data.push(value)
-          //console.log(data);
+          console.log(data);
         }
 
       });
@@ -33,15 +56,10 @@ Users.on('value', function(snapshot){
   //var x = users.find(user => user.UID === id)
 
 });
-Users.on('child_added', function(snapshot){
-  data.push(snapshot.val())
-  var x = data.find(user => user.UID === 12312412412)
-  if(x)
-  {
-    console.log("Hello world");
-  }
-  console.log(x);
-});
+*/
+
+
+
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello Node.js</h1>');
@@ -61,3 +79,14 @@ app.get('/index', function (req, res) {
 app.listen(port, function() {
     console.log('Starting node.js on port ' + port);
 });
+
+function initData(){
+
+
+
+setTimeout(function (){console.log(data);},4000)
+
+
+
+
+}
